7c199d3133cc3f0bf51aaf55b5e8657dd82aac91
RobertT122 <robert@tetrault.org>
2022-04-07 18:19:07 -0400

    Add assets and test canvas element

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,11 @@
-import * as _ from 'lodash';
+const createComponent = () => {
 
-function component() {
   const element = document.createElement('div');
+  const canvas = document.createElement('canvas');
+  element.appendChild(canvas)
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  return element;
+  return canvas;
+  
 }
 
-document.body.appendChild(component());
+document.body.appendChild(createComponent());
