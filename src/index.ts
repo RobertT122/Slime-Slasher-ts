c62f48e08a8d15894c08e54435b581b35069f747
RobertT122 <robert@tetrault.org>
2022-04-10 17:31:51 -0400

    Create build html functions with a custom return type

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,31 @@
-const createComponent = () => {
 
-  const element = document.createElement('div');
-  const canvas = document.createElement('canvas');
-  element.appendChild(canvas)
-
-  return canvas;
-  
+interface GameElements {
+  ctx: CanvasRenderingContext2D;
+  canvas: HTMLCanvasElement;
+  container:HTMLDivElement;
 }
 
-document.body.appendChild(createComponent());
+function buildHTML ():GameElements {
+  const container = document.createElement('div');
+  container.classList.add("game-container")
+  const canvas = document.createElement('canvas');
+  container.appendChild(canvas)
+  document.body.appendChild(container);
+  return {ctx:canvas.getContext("2d"), canvas, container};
+}
+
+function createComponent (): void {
+  
+  const {ctx, canvas, container} = buildHTML();
+  
+
+  // step one:
+    // Draw a single frame of the slime
+  // step two: 
+    // Redraw the frame of the slime at an interval
+  // Once Canvas is working properly add its context to an instance of a game object that will do the same thing;
+}
+
+
+
+window.addEventListener("DOMContentLoaded", createComponent)
