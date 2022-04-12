d6f78cbf41c3b15bd0ecaee519256d1c653dc55b
RobertT122 <robert@tetrault.org>
2022-04-12 11:43:00 -0400

    css hax

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,39 @@
 import {spritePath} from './scripts/helpers'
 
+const css = `
+@import url('https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap');
+
+@media (min-aspect-ratio: 5/8) and (max-height: 1080px){
+  #canvas {
+    width:100%;
+    max-width: 600px;
+  }
+}
+
+@media (min-aspect-ratio: 5/8) and (min-height: 1080px){
+  #canvas {
+    width: 100%;
+    max-width: 800px;
+  }
+}
+
+@media (max-aspect-ratio: 5/8){
+  #canvas {
+    width: 100%;
+    height: 100%
+  }
+}
+
+#game-box{
+  display: flex;
+  justify-content: center;
+}
+
+.grey-background{
+  background-color: lightgrey;
+}
+`;
+
 interface GameElements {
   ctx: CanvasRenderingContext2D;
   canvas: HTMLCanvasElement;
@@ -7,13 +41,20 @@ interface GameElements {
 }
 
 function buildHTML ():GameElements {
+  const style = document.createElement('style');
+  style.innerHTML = css;
+  document.head.appendChild(style);
+
   const container = document.createElement('div');
-  container.classList.add("game-container");
-  container.style.width = "100vw";
-  container.style.height = "100vh";
-  const canvas = document.createElement('canvas');
-  container.appendChild(canvas)
+  container.id = "game-box";
   document.body.appendChild(container);
+
+  const canvas = document.createElement('canvas');
+  canvas.id = "canvas"
+  canvas.width=800;
+  canvas.height=1420;
+  container.appendChild(canvas)
+
   return {ctx:canvas.getContext("2d"), canvas, container};
 }
 
@@ -26,8 +67,6 @@ function createComponent (): void {
 
   image.onload = () => ctx.drawImage(image, 0,0);
   
-  // step one:
-    // Draw a single frame of the slime
   // step two: 
     // Redraw the frame of the slime at an interval
   // Once Canvas is working properly add its context to an instance of a game object that will do the same thing;
